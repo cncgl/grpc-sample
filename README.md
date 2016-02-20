863cb5084c106ad2bdf389e8ebcfdd0dc800be38
cncgl <concigel@gmail.com>
2016-02-20 16:27:34 +0900

    add ruby client

--- README.md
+++ README.md
@@ -35,14 +35,24 @@ $ protoc -I proto --ruby_out=lib --grpc_out=lib --plugin=protoc-gen-grpc=`which 
 ## Execute
 
 サーバー側
+
+### Go の場合
 ```
 $ go run hello_server.go
 ```
 
 クライアント側
+
+### Go の場合
 ```
 $ go run hello_client.go
 ```
+
+### Ruby の場合
+```
+$ ruby hello_client.rb
+```
+
 
 ## License
 [MIT](LICENSE)
--- hello_client.rb
+++ hello_client.rb
@@ -1,18 +1,19 @@
 #!/usr/bin/env ruby
 
 this_dir = File.expand_path(File.dirname(__FILE__))
-lib_dir = File.join(File.dirname(this_dir), 'lib')
+lib_dir = File.join(this_dir, 'lib')
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
-$LOAD_PATH.unshift(this_dir) unless $LOAD_PATH.include?(this_dir)
+# $LOAD_PATH.unshift(this_dir) unless $LOAD_PATH.include?(this_dir)
 
 require 'grpc'
-require 'helloworld'
+require 'helloworld_services'
 
 def main
-  stub = Helloworld::Stub.new
-  GRPC.logger.info("... connecting insecurely")
+  stub = Helloworld::Greeter::Stub.new('localhost:5000', :this_channel_is_insecure)
+  GRPC.logger.info('... connecting insecurely')
 
-  stub.
+  message = stub.say_hello(Helloworld::HelloRequest.new(name: 'cncgl')).message
+  p "Greeting: #{message}"
 end
 
 main
